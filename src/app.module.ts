a723dfef3c5cec2e3211a490ab88e0e6d565320c
Bruno Oliveira <brunohashtagfox@gmail.com>
2022-07-30 00:06:13 -0300

    fé

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { HealthModule } from './health/health.module';
-//import { join } from 'path';
 import { UserModule } from './user/user.module';
 
 @Module({
