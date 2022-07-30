7f9ce2e1c63bf2f3fa47f7e9177435ac47d249fe
Bruno Oliveira <brunohashtagfox@gmail.com>
2022-07-30 02:34:09 -0300

    foi agora

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,10 @@
+import { BaseCollection } from 'src/common/base.entity';
+import { Column } from 'typeorm';
+
+export class User extends BaseCollection {
+	@Column({ unique: true })
+	email: string;
+
+	@Column()
+	name: string;
+}
