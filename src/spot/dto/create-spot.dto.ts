70a95cbb4eb410d368b1cb5112cd7db4151cc4d3
Bruno Oliveira <brunohashtagfox@gmail.com>
2023-01-24 17:34:24 -0300

    alteração

--- src/spot/dto/create-spot.dto.ts
+++ src/spot/dto/create-spot.dto.ts
@@ -1,12 +1,4 @@
-import {
-    IsDateString,
-    IsNotEmpty,
-    IsString,
-} from 'class-validator';
-
 export class CreateSpotDto {
-    @IsNotEmpty({ message: 'Id is required' })
     id: string;
-
     date: Date;
 }
